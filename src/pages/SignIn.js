1e20c9539c5d860a6c66f0c68fa0484577e8ab54
kisabellecortez <kisabellecortez@gmail.com>
2023-12-10 02:20:51 -0500

    added cloud animation in sign in page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -4,11 +4,20 @@ import { AuthContextProvider } from '../context/AuthContext.js'
 export default function SignIn(){
     return(
       <div className="signIn">
-        <h1>Welcome to DiaryVerse</h1>
-        <p>Where every thought will be remembered through the pages of your diary. Begin your narrative today, and watch as each entry becomes a vibrant stroke in the masterpiece of your life.</p>
+        <div className="cloud cloud1"></div>
+        <div className="cloud cloud2"></div>
+        <div className="cloud cloud3"></div>
+        <div className="cloud cloud4"></div>
+        <div className="cloud cloud5"></div>
+        <div className="cloud cloud6"></div>
+        <div className="cloud cloud7"></div>
+        <h1>Welcome to SkyDiary</h1>
+        <p>Begin your narrative today, and watch as each entry becomes a vibrant stroke in the masterpiece of your life.</p>
+        <div className="signIn-card">
         <h1>Sign In</h1>
         <div className="signIn-button">
           <GoogleButton/>
+        </div>
         </div>
       </div>
     );
